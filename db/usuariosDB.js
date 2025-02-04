c225ae632cfb348603edb85cd38d123ffb66e8ba
kensgg <g03.gudicruz@gmail.com>
2025-02-03 18:31:51 -0600

    Proyecto Final Parcial 1

--- db/usuariosDB.js
+++ db/usuariosDB.js
@@ -38,5 +38,77 @@ export const login = async ({ username, password }) => {
     }
 };
 
+export const obtenerUsuarios = async () => {
+    try {
+        const usuarios = await User.find(); 
+        if (usuarios.length === 0) {
+            return mensaje(404, "No hay usuarios registrados");
+        }
+        return mensaje(200, "Usuarios encontrados", "", usuarios);  
+    } catch (error) {
+        return mensaje(400, "Error al obtener los usuarios", error);
+    }
+};
 
+export const usuarioPorId =  async (id) => {
+    try {
+        const usuario = await User.findById(id);
+        if(!usuario){
+            return mensaje(404, "No hay un usuario con ese id");
+        }
+        return mensaje(200, "Usuario encontrado", "", usuario);
+    } catch (error) {
+        return mensaje(400, "Error al obtener usuario", error);
+    }
+}
+
+export const borrarPorId = async (id) => {  
+    try {
+        const usuario = await User.findById(id);
+    if (!usuario) {
+        return mensaje(400, "El usuario no existe");
+    }
+    const usuarioEliminado = await User.findByIdAndDelete(id);
+    if(!usuarioEliminado) {
+        return mensaje(404, "No se ha podido eliminar al usuario");
+    }
+    return mensaje(200, "Usuario borrado correctamente");
+    } catch (error) {
+        return mensaje(400, "No se ha podido eliminar al usuario", error);
+    }
+    
+}
+
+export const actualizarPorId = async ({ id, username, password }) => {
+    try {
+        const usuario = await User.findById(id);
+        if (!usuario) {
+            return mensaje(400, "El usuario no existe");
+        }
+
+        const usuarioExistente = await User.findOne({ username });
+        if (usuarioExistente && usuarioExistente._id.toString() !== id) {
+            return mensaje(400, "El nombre de usuario ya está en uso");
+        }
+
+        if (password) {
+            const { salt, hash } = encriptarPassword(password);
+            usuario.password = hash;
+            usuario.salt = salt;
+        }
+
+        if (username) {
+            usuario.username = username;
+        }
+
+        const usuarioActualizado = await usuario.save();
+        if (!usuarioActualizado) {
+            return mensaje(404, "No se ha podido actualizar al usuario");
+        }
+
+        return mensaje(200, "Usuario actualizado correctamente");
+    } catch (error) {
+        return mensaje(400, "Error al actualizar al usuario", error);
+    }
+};
 
--- routes/usuariosRutas.js
+++ routes/usuariosRutas.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { login, register } from "../db/usuariosDB.js";
+import { login, register, obtenerUsuarios, usuarioPorId, borrarPorId, actualizarPorId } from "../db/usuariosDB.js";
 const router = Router();
 
 router.post("/registro", async(req, res)=>{
@@ -13,6 +13,31 @@ router.post("/login", async(req, res)=>{
     console.log(respuesta.mensajeOriginal);
     res.cookie("token", respuesta.token).status(respuesta.status).json(respuesta.mensajeUsuario);
 });
+
+router.get("/usuarios", async (req, res) => {
+    const respuesta = await obtenerUsuarios();
+    res.status(respuesta.status).json({ mensaje: respuesta.mensajeUsuario, usuarios: respuesta.token });
+});
+
+router.get("/usuarioPorId/:id", async (req, res) => { 
+    const { id } = req.params;  
+    const respuesta = await usuarioPorId(id);
+    res.status(respuesta.status).json({ mensaje: respuesta.mensajeUsuario, usuario: respuesta.token });
+});
+
+router.delete("/eliminarPorId/:id", async (req, res) => { 
+    const { id } = req.params;  
+    const respuesta = await borrarPorId(id);  
+    res.status(respuesta.status).json(respuesta.mensajeUsuario); 
+});
+
+router.put("/actualizarPorId/:id", async (req, res) => {  
+    const { id } = req.params; 
+    const { username, email, password } = req.body; 
+    const respuesta = await actualizarPorId({ id, username, email, password });
+    res.status(respuesta.status).json(respuesta.mensajeUsuario); 
+});
+
 
 router.get("/salir", async(req, res)=>{
     res.json("Adios, Estás en salir");
